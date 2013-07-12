4f2cd229b119cd17594dd4aa714c6a53250065b1
Thomas Wilburn <news@thomaswilburn.net>
2013-07-11 21:50:07 -0700

    Packaged app now correctly initialized, refocused based on current Chrome OS API limitations.

--- maker.js
+++ maker.js
@@ -27,9 +27,26 @@ Maker.controller("ManifestController", ["$scope", function($scope) {
   $scope.image = false;
   
   $scope.generate = function() {
-    var json = JSON.stringify($scope.manifest);
-    console.log(json);
+    var manifest = $scope.manifest;
+    manifest.app.launch.urls = [ manifest.app.launch.web_url ];
+    //save the image out to the chosen directory
+    //can we use a data URL?
+    //
+    var json = JSON.stringify(manifest);
+    
+    chrome.fileSystem.chooseEntry({
+      type: "saveFile"
+    }, function(fileEntry) {
+      console.log(arguments);
+      window.file = fileEntry;
+    });
   }
+  
+  var imageFileInput = document.querySelector("#imageFileInput");
+  imageFileInput.addEventListener("change", function() {
+    var file = this.files.pop();
+    $scope.manifest.icons["128"] = file.name;
+  });
   
 }]);
 
